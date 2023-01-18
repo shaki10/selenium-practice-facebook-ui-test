e22fbbda7a789f2012cbe471a7597bd53f5a60dc
shaki10 <shakiahmed10@gmail.com>
2023-01-18 17:07:41 -0500

    commit: Add new feature file

---
+++ src/main/java/animal/Animal.java
@@ -0,0 +1,12 @@
+package animal;
+
+public  abstract class Animal {
+    int a =10;
+    boolean b = true;
+
+
+     abstract int cow();
+     abstract int leg();
+
+
+}
---
+++ src/main/java/animal/Dog.java
@@ -0,0 +1,13 @@
+package animal;
+
+public class Dog extends Animal{
+    @Override
+    int cow() {
+        return 0;
+    }
+
+    @Override
+    int leg() {
+        return 0;
+    }
+}
